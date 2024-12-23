1beb5106f17c87ffb465ae6cb297da4e0bdec554
Илья Смоленский <smolenskiy20030409@mail.ru>
2024-12-23 19:10:42 +0300

    docker ports

--- kino/__init__.py
+++ kino/__init__.py
@@ -11,8 +11,8 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://u2926938_default:w4LyTqR9np0B2fWK@localhost:3306/u2926938_flask_kino'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@host.docker.internal:5432/flask_kino'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost:5432/flask_kino'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@host.docker.internal:5432/flask_kino'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost:5432/flask_kino'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
